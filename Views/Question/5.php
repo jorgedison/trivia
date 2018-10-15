ba2bd673d51c792133aa25d5ce6888bdb11f9edd
Jorge Rodriguez <jorgeedison1@gmail.com>
2018-10-15 02:39:28 -0500

    validate form

--- Views/Question/5.php
+++ Views/Question/5.php
@@ -1,26 +1,10 @@
 
-	<link href="Keyboard/docs/css/jquery-ui.min.css" rel="stylesheet">
-	<!-- still using jQuery v2.2.4 because Bootstrap doesn't support v3+ -->
-	<script src="Keyboard/docs/js/jquery-latest.min.js"></script>
-	<script src="Keyboard/docs/js/jquery-ui.min.js"></script>
-	<!-- <script src="docs/js/jquery-migrate-3.0.0.min.js"></script> -->
+  <link rel="stylesheet" type="text/css" href="jquery.ml-keyboard.css">
+  <!--<link rel="stylesheet" type="text/css" href="demo.css">-->
 
-	<!-- keyboard widget css & script (required) -->
-	<link href="Keyboard/css/keyboard.css" rel="stylesheet">
-	<script src="Keyboard/js/jquery.keyboard.js"></script>
-
-
-
-	<!-- demo only -->
-	<link rel="stylesheet" href="Keyboard/docs/css/bootstrap.min.css">
-	<link rel="stylesheet" href="Keyboard/docs/css/font-awesome.min.css">
-	<link href="Keyboard/docs/css/demo.css" rel="stylesheet">
-	<link href="Keyboard/docs/css/tipsy.css" rel="stylesheet">
-	<link href="Keyboard/docs/css/prettify.css" rel="stylesheet">
-	<script src="Keyboard/docs/js/bootstrap.min.js"></script>
-	<script src="Keyboard/docs/js/demo.js"></script>
-	<script src="Keyboard/docs/js/jquery.tipsy.min.js"></script>
-	<script src="Keyboard/docs/js/prettify.js"></script> <!-- syntax highlighting -->
+  <script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
+  <script src="jquery.ml-keyboard.js"></script>
+  <script src="demo.js"></script>
 
 
 
@@ -36,7 +20,7 @@ width: 840px;
     border: none;
     border-color: white;
     color: #222222;
-    outline: none;
+    outline: none;   
 }
 
 .cajatexto_2 {
@@ -68,22 +52,62 @@ width: 840px;
     outline: none;
 }
 
+.img {
+    position: fixed;
+    left: 0px;
+    top: 0px;
+    z-index: -100;
+}
+
 </style>
 
 <script type="text/javascript">
 function timeout_init() {
     setTimeout('timeout_trigger()', 2000);
 }
+
+function validateForm()
+{
+  var fields = ["nombres", "correo", "celular"]
+    var x = document.forms["form"]["nombres"].value;
+    var y = document.forms["form"]["correo"].value;
+    var z = document.forms["form"]["celular"].value;
+
+    if (x == "") {
+        bootbox.alert("Ingrese Nombres y Apellidos");
+        return false;
+    } else if (y == "") {
+    	alert("Ingrese Correo");
+        return false;
+    } else if (!y.match(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)){
+    	alert("Correo invalido");
+        return false;
+    } else if (z == "") {
+    	alert("Ingrese Celular");
+        return false;
+    } else if (!z.match(/^\d+/)){
+    	alert("Celular debe tener numeros");
+        return false;
+    } else{
+    	var s = document.getElementsByTagName("form");
+		s[0].submit();// Form submission
+
+    }
+    }
+
 </script>
 
-<form name="form" action="?controller=question&&action=cHJlZ3VudGFzZWlz" method="POST">
-<input type="text" class="cajatexto_1" id="nombres" name="nombres" maxlength="50"  required>
-<input type="email" class="cajatexto_2" id="correo" name="correo" maxlength="30"  required pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$">
-<input type="text" pattern="\d*" class="cajatexto_3" id="celular" name="celular" maxlength="9" required >
+
+<div>
+<form name="form" action="?controller=question&&action=cHJlZ3VudGFzZWlz" method="POST" autocomplete="off" id="form">
+	<input type="text" class="cajatexto_1" id="example-1" name="nombres" maxlength="50"  required/>
+    <input type="email" class="cajatexto_2" id="example-1" name="correo" maxlength="50"  required/>
+    <input type="text" class="cajatexto_3" id="example-1" name="celular" maxlength="9" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" required/>
 
 <img src="assets/images/6.png" usemap="#image-map">
 
 <map name="image-map">
-    <area target="" alt="" title="" onClick="form.submit(); setTimeout('timeout_trigger()', 2000);" coords="336,1459,736,1559" shape="rect">
+    <area target="" alt="" title="" onClick="validateForm()" coords="336,1459,736,1559" shape="rect">
 </map>
 </form>
+</div>
